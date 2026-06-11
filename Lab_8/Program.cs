70e16857c1f9785b6cdbeaf2a9ebc17559ff7730
IgorKulduaev <and.sobol39@mail.ru>
2026-06-11 09:14:17 +0200

    Add files via upload

---
+++ Lab_8/Program.cs
@@ -0,0 +1,20 @@
+﻿Stack<int> stack = new Stack<int>();
+int n = new Random().Next(10, 20);
+
+for (int i = 0; i < n; i++)
+
+{
+
+    stack.Push(new Random().Next(10, 100));
+
+}
+
+foreach (int i in stack) Console.Write(i + " ");
+
+Console.WriteLine();
+
+double s = 0;
+
+foreach (int i in stack) if (i % 2 == 0) s += i;
+
+Console.WriteLine($"Avg={(s / stack.Count):F2}");
